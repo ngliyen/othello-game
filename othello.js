9b5e257d873824cc9a72fe5d5e355e308d29680d
Yucheng Low <2740522+ylow@users.noreply.github.com>
2022-12-17 15:35:25 -0800

    more initial

--- othello.js
+++ othello.js
@@ -1,15 +1,19 @@
 console.log("Othello");
 document.body.innerHTML += "<br>boo<br>moof"
 
+const BLACK = 1;
+const WHITE = -1;
+const EMPTY = 0;
 class OthelloBoard {
   constructor() {
     this.board = new Array(8);
     for (var i = 0; i< 8; ++i) {
       this.board[i] = new Array(8)
       for (var j = 0; j< 8; ++j) {
-        this.board[i][j] = 0;
+        this.board[i][j] = EMPTY;
       }
     }
+    this.curplayer = BLACK;
   }
 
   print() {
@@ -25,6 +29,7 @@ class OthelloBoard {
   }
 
   final_score() {
+    return this.curplayer;
   }
 }
 console.log(new OthelloBoard)
