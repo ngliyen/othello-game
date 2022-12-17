294fc0b5e29c08395c749759d01e61da7bb06d39
Anthony Moh <anmoh@microsoft.com>
2022-12-17 15:57:08 -0800

    Initialize board state

--- othello.js
+++ othello.js
@@ -3,16 +3,21 @@
 const BLACK = 1;
 const WHITE = -1;
 const EMPTY = 0;
+const BOARDSIZE = 8;
 class OthelloBoard {
   constructor() {
-    this.board = new Array(8);
-    for (var i = 0; i< 8; ++i) {
-      this.board[i] = new Array(8)
-      for (var j = 0; j< 8; ++j) {
+    this.board = new Array(BOARDSIZE);
+    for (var i = 0; i< BOARDSIZE; ++i) {
+      this.board[i] = new Array(BOARDSIZE)
+      for (var j = 0; j< BOARDSIZE; ++j) {
         this.board[i][j] = EMPTY;
       }
     }
     this.curplayer = BLACK;
+    this.board[BOARDSIZE/2][BOARDSIZE/2] = BLACK;
+    this.board[BOARDSIZE/2-1][BOARDSIZE/2-1] = BLACK;
+    this.board[BOARDSIZE/2-1][BOARDSIZE/2] = WHITE;
+    this.board[BOARDSIZE/2][BOARDSIZE/2-1] = WHITE;
   }
 
   print() {
@@ -37,14 +42,6 @@ class OthelloBoard {
     return this.curplayer;
   }
 
-  final_score(player) {
-    console.assert(player == BLACK || player == WHITE)
-    count = 0;
-    for (var i = 0; i< 8; ++i) {
-      for (var j = 0; j< 8; ++j) {
-        count += this.board[i][j]==player
-      }
-    }
-    return count;
+  final_score() {
   }
 }
