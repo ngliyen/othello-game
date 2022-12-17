27c40461431badb653d3cb1b3be0a4af0ee7e021
Yucheng Low <2740522+ylow@users.noreply.github.com>
2022-12-17 15:30:02 -0800

    initial setup

--- othello.js
+++ othello.js
@@ -1,2 +1,15 @@
 console.log("Othello");
 document.body.innerHTML += "<br>boo<br>moof"
+
+class OthelloBoard {
+  constructor() {
+    this.board = new Array(8);
+    for (var i = 0; i< 8; ++i) {
+      this.board[i] = new Array(8)
+      for (var j = 0; j< 8; ++j) {
+        this.board[i][j] = 0;
+      }
+    }
+  }
+}
+console.log(new OthelloBoard)
