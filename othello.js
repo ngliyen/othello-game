bd1dffe7a03bc0894f58f1b90e3e79466e440de8
Yucheng Low <2740522+ylow@users.noreply.github.com>
2022-12-17 15:53:41 -0800

    relative score

--- othello.js
+++ othello.js
@@ -43,6 +43,7 @@ class OthelloBoard {
     for (var i = 0; i< 8; ++i) {
       for (var j = 0; j< 8; ++j) {
         count += this.board[i][j]==player
+        count -= this.board[i][j]==-player
       }
     }
     return count;
