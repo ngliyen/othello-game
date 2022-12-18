7ff8fdcb85393c49b0c2ccd24cfd6f71b1869b9f
Anthony Moh <anmoh@microsoft.com>
2022-12-17 16:19:10 -0800

    Canvas board

--- othello.js
+++ othello.js
@@ -21,15 +21,25 @@ class OthelloBoard {
   }
 
   print() {
-    var board = document.getElementById("board");
-    var boardBody = "";
+    const SQUARESIZE = 30;
+    var canvas = document.getElementById("board");
+    var ctx = canvas.getContext('2d');
+    ctx.fillStyle = 'green';
+
     for (var i = 0; i < this.board.length; i++) {
        for (var j = 0; j < this.board[0].length; j++) {
-         boardBody += this.board[i][j];
+         ctx.clearRect(i*SQUARESIZE, j*SQUARESIZE, SQUARESIZE, SQUARESIZE);
+         ctx.strokeRect(i*SQUARESIZE, j*SQUARESIZE, SQUARESIZE, SQUARESIZE);
+         if (this.board[i][j] == BLACK){
+           ctx.fillStyle = 'black';
+           ctx.fillRect(i*SQUARESIZE + 5, j*SQUARESIZE + 5, SQUARESIZE - 10, SQUARESIZE - 10);
+         }
+         if (this.board[i][j] == WHITE){
+           ctx.fillStyle = 'grey';
+           ctx.fillRect(i*SQUARESIZE + 5, j*SQUARESIZE + 5, SQUARESIZE - 10, SQUARESIZE - 10);
+         }
        }
-       boardBody += "<br>";
     }
-    board.innerHTML = boardBody;
   }
 
   gen_moves() {
